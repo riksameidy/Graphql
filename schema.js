e44f6b36a9e66bb29cacb89c36af081a5bd285b5
riksameidy <riksameidy@gmail.com>
2017-08-16 07:27:13 +0700

    finish part 3

--- schema.js
+++ schema.js
@@ -7,6 +7,52 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
+//Hardcoded data
+const customers = [
+  {id:'1',name:'Riksa Meidy',email:'rmk@gmail.com',age:22},
+  {id:'2',name:'Gugun Guntur',email:'gg99@gmail.com',age:22},
+  {id:'3',name:'Amalya Citra',email:'acp@gmail.com',age:20},
+];
 
+//CustomerType
+const CustomerType = new GraphQLObjectType({
+  name:'Customer',
+  fields: ()=>({
+    id:{type:GraphQLString},
+    name:{type:GraphQLString},
+    email:{type:GraphQLString},
+    age:{type:GraphQLInt},
+
+  })
+});
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+  name:'RootQueryType',
+  fields:{
+    customer: {
+      type:CustomerType,
+      args:{
+        id:{type:GraphQLString}
+      },
+      resolve(parentValue,args){
+        for(let i=0; i<customers.length;i++){
+          if(customers[i].id == args.id){
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers:{
+      type:new GraphQLList(CustomerType),
+      resolve(parentValue,args){
+        return customers;
+      }
+    }
+  }
+
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
